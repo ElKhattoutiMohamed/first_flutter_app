8a5e9a81cbd98662da126f1b9cb570f2c047c697
El Khattouti Mohamed Amine <elkhattoutimohamed99@gmail.com>
2022-09-20 14:40:37 +0200

    WIP : second page

--- second_app/lib/home_page.dart
+++ second_app/lib/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:second_app/learn_flutter_page.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -6,8 +7,14 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child:
-          ElevatedButton(onPressed: () {}, child: const Text("Learn Flutter")),
+      child: ElevatedButton(
+          onPressed: () {
+            Navigator.of(context)
+                .push(MaterialPageRoute(builder: (BuildContext context) {
+              return const LearnFlutterPage();
+            }));
+          },
+          child: const Text("Learn Flutter")),
     );
   }
 }
--- second_app/lib/learn_flutter_page.dart
+++ second_app/lib/learn_flutter_page.dart
@@ -1,1 +1,56 @@
+import 'package:flutter/material.dart';
 
+class LearnFlutterPage extends StatefulWidget {
+  const LearnFlutterPage({Key? key}) : super(key: key);
+
+  @override
+  State<LearnFlutterPage> createState() => _LearnFlutterPageState();
+}
+
+class _LearnFlutterPageState extends State<LearnFlutterPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Learning Page"),
+        automaticallyImplyLeading: false,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back_ios),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+      ),
+      body: Column(
+        children: [
+          Image.asset('assets/images/40.png'),
+          const SizedBox(
+            height: 10,
+          ),
+          const Divider(
+            color: Colors.black,
+          ),
+          Container(
+            width: double.infinity,
+            margin: const EdgeInsets.all(10),
+            padding: const EdgeInsets.all(10),
+            color: Colors.purple,
+            child: const Text(
+              "Ener",
+              style: TextStyle(color: Colors.white),
+              textAlign: TextAlign.center,
+            ),
+          ),
+          const Divider(
+            color: Colors.black,
+          ),
+          ElevatedButton(
+              onPressed: () {
+                debugPrint('Bolt pressed');
+              },
+              child: const Text("Bolt"))
+        ],
+      ),
+    );
+  }
+}
