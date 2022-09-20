37f18694f47372e0c3c5374b12e2de65e1b8fcfd
El Khattouti Mohamed Amine <elkhattoutimohamed99@gmail.com>
2022-09-20 15:42:45 +0200

    finished flutter page

--- second_app/lib/learn_flutter_page.dart
+++ second_app/lib/learn_flutter_page.dart
@@ -8,10 +8,25 @@ class LearnFlutterPage extends StatefulWidget {
 }
 
 class _LearnFlutterPageState extends State<LearnFlutterPage> {
+  bool lights = true;
+  bool isSwitched = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        actions: [
+          IconButton(
+              onPressed: () {
+                debugPrint("Appbar button pressed");
+              },
+              icon: const Icon(Icons.phone_iphone)),
+          IconButton(
+              onPressed: () {
+                debugPrint("Appbar button pressed");
+              },
+              icon: const Icon(Icons.phone_android))
+        ],
         title: const Text("Learning Page"),
         automaticallyImplyLeading: false,
         leading: IconButton(
@@ -21,35 +36,80 @@ class _LearnFlutterPageState extends State<LearnFlutterPage> {
           },
         ),
       ),
-      body: Column(
-        children: [
-          Image.asset('assets/images/40.png'),
-          const SizedBox(
-            height: 10,
-          ),
-          const Divider(
-            color: Colors.black,
-          ),
-          Container(
-            width: double.infinity,
-            margin: const EdgeInsets.all(10),
-            padding: const EdgeInsets.all(10),
-            color: Colors.purple,
-            child: const Text(
-              "Ener",
-              style: TextStyle(color: Colors.white),
-              textAlign: TextAlign.center,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.asset('assets/images/40.png'),
+            const SizedBox(
+              height: 10,
             ),
-          ),
-          const Divider(
-            color: Colors.black,
-          ),
-          ElevatedButton(
-              onPressed: () {
-                debugPrint('Bolt pressed');
+            const Divider(
+              color: Colors.black,
+            ),
+            Container(
+              width: double.infinity,
+              margin: const EdgeInsets.all(10),
+              padding: const EdgeInsets.all(10),
+              color: Colors.purple,
+              child: const Text(
+                "Ener",
+                style: TextStyle(color: Colors.white),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            const Divider(
+              color: Colors.black,
+            ),
+            ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    primary: isSwitched ? Colors.yellow : Colors.purple),
+                onPressed: () {
+                  debugPrint('Bolt pressed');
+                },
+                child: const Text("Bolt")),
+            OutlinedButton(
+                onPressed: () {
+                  debugPrint('Bolt Outlined pressed');
+                },
+                child: const Text("Bolt")),
+            TextButton(
+                onPressed: () {
+                  debugPrint('Bolt Button pressed');
+                },
+                child: const Text("Bolt")),
+            GestureDetector(
+              onTap: () {
+                setState(() {
+                  lights = !lights;
+                });
               },
-              child: const Text("Bolt"))
-        ],
+              child: Container(
+                color: lights ? Colors.yellow : Colors.transparent,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: const [
+                    Icon(Icons.lightbulb_outline),
+                    Text("Less power this"),
+                    Icon(Icons.lightbulb_outline),
+                  ],
+                ),
+              ),
+            ),
+            Switch(
+                value: isSwitched,
+                onChanged: (bool value) {
+                  isSwitched = !isSwitched;
+                  setState(() {
+                    lights = value;
+                  });
+                }),
+            const Divider(
+              thickness: 5,
+              color: Colors.black,
+            ),
+            Image.asset('assets/images/01.png'),
+          ],
+        ),
       ),
     );
   }
